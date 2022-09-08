18ea8d0c817d3219da538699064cc10102db3c76
7196Pac <ovieemmanuel300@gmail.com>
2022-09-08 16:50:13 -0700

    kk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,13 +5,10 @@
  *   * Return: 0 if success.
  * 
 */
-
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
