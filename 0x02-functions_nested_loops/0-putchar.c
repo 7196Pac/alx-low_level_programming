652068b965af7e32c57374b9904e55767497a835
7196Pac <ovieemmanuel300@gmail.com>
2022-09-13 07:16:32 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
-
-
-#include "main.h"
-
-/**
- * main - entry point
- *
- * Description: Prints Holberton with _putchar
- *
- * Return: always 0
- */
-
-int main(void)
-{
-	int i = 0;
-	char c;
-	char s[] = "_putchar\n";
-
-	while (i <= 8)
-	{
-		c = s[i];
-		_putchar(c);
-		i++;
-	}
-	return (0);
-}
+#include"main.h" 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Description: prints _putchar using putchar prototype 
+  * 
+  * Return: Always 0 (Success) 
+ */ 
+  
+ int main(void) 
+ { 
+         char str[] = "_putchar"; 
+         int ch; 
+  
+         for (ch = 0; ch < 8; ++ch) 
+                 _putchar(str[ch]); 
+         _putchar('\n'); 
+  
+         return (0); 
+ }
